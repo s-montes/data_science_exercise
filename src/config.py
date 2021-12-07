8715670a046137b681833dbc948e20d0c9f333a6
Sebastian Montes <montes.valencia.sebastian@gmail.com>
2021-12-07 22:34:33 +0100

    FEAT: statistical tests

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ fpath = Path(__file__).parents[1].absolute()
 
 
 class ProjectConfig(BaseSettings):
-    db_path: str = str(fpath / Path("data/Data scientist exercise.db"))
+    db_path: Path = fpath / Path("data/Data scientist exercise.db")
     raw_records: str = "access_log"
     clean_records: str = "clean_log"
     random_seed: int = 1234
